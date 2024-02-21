259c29175fb56aea570af5ffbcb57a387d9b6b9a
unknown <saepuladi4@gmail.com>
2024-02-21 09:24:39 +0700

    comment: some import not use

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from "./components/Navbar.tsx"
-import { Routes } from 'react-router'
+// import { Routes } from 'react-router'
 
 function App() {
   return (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 // import { Link } from "react-router-dom";  // Import Link from React Router
 import { MenuItems } from "./MenuItems";
 import "./NavbarStyle.css";
